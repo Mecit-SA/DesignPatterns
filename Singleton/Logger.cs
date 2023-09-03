e1e2e33cfa7e192accccbb7eb39fb8f34aeb594f
mecit <mecitsariguzel@gmail.com>
2023-09-03 13:18:49 +0300

    Singleton Added

---
+++ Singleton/Logger.cs
@@ -0,0 +1,25 @@
+﻿namespace Singleton
+{
+    class Logger
+    {
+        private string _fileName;
+        private static IDictionary<string, Logger> _instances = new Dictionary<string, Logger>();
+
+        public static Logger GetInstance(string fileName)
+        {
+            if (!_instances.ContainsKey(fileName))
+                _instances.Add(fileName, new Logger(fileName));
+
+            return _instances[fileName];
+        }
+
+        private Logger(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public void Log(string message) => Console.WriteLine(message);
+
+        public static int InstanceCount => _instances.Count;
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿using Singleton;
+
+var logger = Logger.GetInstance("default.txt");
+var logger2 = Logger.GetInstance("file.txt");
+var logger3 = Logger.GetInstance("file2.txt");
+var logger4 = Logger.GetInstance("file.txt");
+var logger5 = Logger.GetInstance("file.txt");
+
+Console.WriteLine("Instance Count:" + Logger.InstanceCount);
